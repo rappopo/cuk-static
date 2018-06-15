27241b6481bad7875dc3b0f6a62d605d66c9d6f1
Ardhi Lukianto <ardhi@lukianto.com>
2018-06-16 06:30:06 +0700

    reg update

--- boot.js
+++ boot.js
@@ -8,15 +8,14 @@ module.exports = function(cuk){
   const { _, fs, path, helper, debug } = cuk.lib
   const { app, mount } = cuk.pkg.http.lib
   const pkg = cuk.pkg[pkgId]
-  const staticRootDir = path.join(cuk.dir.root, 'cuks', pkgId, 'resource')
+  const staticAppDir = path.join(cuk.dir.app, 'cuks', pkgId, 'resource')
 
-  fs.ensureDirSync(staticRootDir)
+  fs.ensureDirSync(staticAppDir)
 
   const serve = (dir, options) => {
     return koaStatic(dir, helper('core:makeOptions')(pkgId, 'options', options))
   }
 
-  pkg.trace('Initializing...')
   pkg.lib.serve = serve
 
   const mountDir = function(kv, root, pid) {
@@ -25,50 +24,49 @@ module.exports = function(cuk){
       if (!path.isAbsolute(v)) {
         let _v = path.join(root, v)
         if (!fs.existsSync(_v))
-          _v = path.join(cuk.dir.root, v)
+          _v = path.join(cuk.dir.app, v)
         v = _v
       }
       if (!fs.existsSync(v)) return
-      let mp = pkg.cfg.common.mountResource + (pid ? `${pid === '/' ? '':pid}` : '') + k
+      let mp = pkg.cfg.common.mount + (pid ? `${pid === '/' ? '':pid}` : '') + k
       if ((pkg.cfg.common.disabled || []).indexOf(mp) > -1) {
-        pkg.trace(`Disabled » ${mp} -> ${helper('core:makeRelDir')(v, cuk.dir.root)}`)
+        helper('core:bootTrace')(`%A Disabled %K ${mp} %L ${helper('core:makeRelDir')(v, cuk.dir.app, 'ADIR:.')}`, null, null, null)
         return
       }
       app.use(mount(mp, serve(v)))
-
-      pkg.trace(`Serve » ${mp} -> ${helper('core:makeRelDir')(v, cuk.dir.root)}`)
+      helper('core:bootTrace')(`%A Enabled %K ${mp} %L ${helper('core:makeRelDir')(v, cuk.dir.app, 'ADIR:.')}`, null, null, null)
     })
   }
 
   return new Promise((resolve, reject) => {
     let faviconDef = path.join(pkg.dir, 'cuks', pkgId, 'resource', 'favicon.ico'),
-      faviconFile = path.join(staticRootDir, 'favicon.ico')
+      faviconFile = path.join(staticAppDir, 'favicon.ico')
     if (!fs.existsSync(faviconFile)) faviconFile = faviconDef
     app.use(favicon(faviconFile))
-    pkg.trace(`Serve » favicon.ico -> %s`, helper('core:makeRelDir')(faviconFile))
+    helper('core:bootTrace')(`%A Enabled %K favicon.ico %L %s`, helper('core:makeRelDir')(null, null, null, faviconFile))
     _.forOwn(cuk.pkg, (v, k) => {
       let dir = path.join(v.dir, 'cuks', pkgId, 'resource'),
-        mp = `${pkg.cfg.common.mountResource}${v.cfg.common.mount === '/' ? '' : v.cfg.common.mount}`
+        mp = `${pkg.cfg.common.mount}${v.cfg.common.mount === '/' ? '' : ('/' + v.id)}`
       if (!fs.existsSync(dir)) return
       if ((pkg.cfg.common.disabled || []).indexOf(mp) > -1) {
-        pkg.trace(`Disabled » ${mp} -> ${helper('core:makeRelDir')(dir)}`)
+        helper('core:bootTrace')(`%A Disabled %K ${mp} %L ${helper('core:makeRelDir')(dir, cuk.dir.app, 'ADIR:.')}`, null, null, null)
         return
       }
-      if (mp === pkg.cfg.common.mountResource) {
+      if (mp === pkg.cfg.common.mount) {
         let mws = [helper('http:composeMiddleware')(_.get(pkg.cfg, 'cuks.http.middleware', []), `${pkg.id}:*`)]
         mws.push(serve(dir))
         app.use(mount(mp, cuk.pkg.http.lib.compose(mws)))
       } else {
         app.use(mount(mp, serve(dir)))
       }
-      pkg.trace(`Serve » ${mp} -> ${helper('core:makeRelDir')(dir)}`)
+      helper('core:bootTrace')(`%A Enabled %K ${mp} %L ${helper('core:makeRelDir')(dir, cuk.dir.app, 'ADIR:.')}`, null, null, null)
     })
     Promise.map(helper('core:pkgs')(), function(p) {
       return new Promise((resv, rejc) => {
         let dir = path.join(p.dir, 'cuks', pkgId)
         helper('core:makeConfig')(dir, 'resource')
         .then(result => {
-          mountDir(result, p.dir, p.cfg.common.mount)
+          mountDir(result, p.dir, '/' + p.id)
           resv(true)
         })
       })
